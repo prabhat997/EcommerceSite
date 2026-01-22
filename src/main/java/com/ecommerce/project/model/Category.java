e149decb788927cb7d30f4e52352a29f0649b844
Prabhat <prabhat.khanduri67@gmail.com>
2026-01-22 12:14:04 +0530

    added LOMBOK in Category.java

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,27 +1,18 @@
 package com.ecommerce.project.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//below are the three annotations using Lombok dependency
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Category {
 
     private  Long categoryId;
     private String categoryName;
 
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
