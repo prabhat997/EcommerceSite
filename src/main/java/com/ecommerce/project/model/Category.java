7fed7bc793468c01707164f1992629d5a0c5a670
Prabhat <prabhat.khanduri67@gmail.com>
2026-01-24 22:45:34 +0530

    added Global Exception package with adding ResponseEntity

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,10 +1,12 @@
 package com.ecommerce.project.model;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//below are the three annotations using Lombok dependency
+//Lombok is a Java library that removes boilerplate code like getters, setters, constructors, toString(),etc.
+// It’s especially useful in Spring Boot projects
 
 @Data
 @NoArgsConstructor
@@ -13,6 +15,8 @@ import lombok.NoArgsConstructor;
 public class Category {
 
     private  Long categoryId;
+
+    @NotBlank
     private String categoryName;
 
 }
