9cd5d077413af3e858f6946bcf1faab5f040965d
Prabhat <prabhat.khanduri67@gmail.com>
2026-03-10 11:04:32 +0530

    adding Products first time

--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -2,9 +2,12 @@ package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Product;
 import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ProductService {
     ProductDTO addProduct(Long categoryId, Product product);
+
+    ProductResponse getAllProducts();
 }
